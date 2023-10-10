b060763b0d7d72aab5739f131ee7ff8ad887b406
night-jpg <2018108001@bcc.unifal-mg.edu.br>
2023-10-10 13:15:21 -0400

    Final Changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import { TiDelete } from 'react-icons/ti';
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -21,7 +22,18 @@ const ExpenseItem = (props) => {
             type: 'ADD_EXPENSE',
             payload: expense
         });
+    }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name, 
+            cost: 10,
+        }
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        })
     }
 
     return (
@@ -29,7 +41,8 @@ const ExpenseItem = (props) => {
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
         <td><img onClick={event=> increaseAllocation(props.name)} src="plus.png" alt="plus-icon"></img></td>
-        <td><img onClick={handleDeleteExpense} src="minus.png" alt="minus-icon"></img></td>
+        <td><img onClick={event => decreaseAllocation(props.name)} src="minus.png" alt="minus-icon"></img></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
